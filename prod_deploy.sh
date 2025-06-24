06619b5eb39dd929c430c59b265e95fba4ee21f7
csterritt <csterritt@gmail.com>
2025-06-24 11:17:27 -0400

    Removed vite.config.js code from prod_deploy.sh since it's unused.

--- prod_deploy.sh
+++ prod_deploy.sh
@@ -11,9 +11,7 @@ git commit -a -m "$msg"
 git checkout main
 git merge --no-commit -s ort -Xtheirs dev || echo "Merge conflicts found, evidently."
 ./clean-for-production.rb ignore || exit 1
-cp vite-no-client.config.ts vite.config.ts
 sleep 1
 curl -s 'http://localhost:3000' > src/index.html || exit 1
-git restore vite.config.ts
 git add src/index.html
 echo Changes brought over, please check and commit.
